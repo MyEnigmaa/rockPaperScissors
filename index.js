7dccea28e52c5c0b9986874d2bd7c8c77988c131
Nik <n.koeck@gmx.de>
2022-12-03 15:15:05 +0100

    add backroundcolor, remove unnecessary code, restructer

--- index.js
+++ index.js
@@ -3,97 +3,108 @@ function getComputerChoice(){ //Generates the Hand based of a random number
     let compHand;
     switch(randomNumber){
         case 0:
-            compHand = "rock";
+            compHand = "Rock";
             break;
         case 1:
-            compHand = "paper";
+            compHand = "Paper";
             break;
         case 2:
-            compHand = "scissors";
+            compHand = "Scissors";
             break;
     }
     return compHand;
 }
 
-function playRound(){ //compares the picks and chooses a result
-    let playerSelection = playerInput();
+function playRound(playerSelection){ //compares the picks and chooses a result
     let computerSelection = getComputerChoice();
-    if(playerSelection == computerSelection)
-        return 2;
+    if(playerSelection == computerSelection){
+        return"Draw!";
+    }
     switch(playerSelection){
-        case "rock":
-            if(computerSelection == "paper"){
-                console.log("Computer has Paper. You lost!");
-                return 1;
+        case "Rock":
+            if(computerSelection == "Paper"){
+                computerWins++;
+                roundsPlayed++;
+                return "Computer has Paper.";
             }
             else{
-                console.log("Computer has Scissors. You Won!");
-                return 0;
+                playerWins++;
+                roundsPlayed++;
+                return"Computer has Scissors.";
             }
             break;
-        case "paper":
-            if(computerSelection == "scissors"){
-                console.log("Computer has Scissors. You lost!");
-                return 1;
+        case "Paper":
+            if(computerSelection == "Scissors"){
+                computerWins++;
+                roundsPlayed++;
+                return"Computer has Scissors.";
             }
             else{
-                console.log("Computer has Rock. You Won!");
-                return 0;
+                playerWins++;
+                roundsPlayed++;
+                return"Computer has Rock.";
             }
             break;
-        case "scissors":
-            if(computerSelection == "rock"){
-                console.log("Computer has Rock. You lost!");
-                return 1;
+        case "Scissors":
+            if(computerSelection == "Rock"){
+                computerWins++;
+                roundsPlayed++;
+                return"Computer has Rock.";
             }
             else{
-                console.log("Computer has Paper. You Won!");
-                return 0;
+                playerWins++;
+                roundsPlayed++;
+                return"Computer has Paper.";
             }
             break;
     }
 }
 
-function playerInput(){ //takes the userinput and fromats it
-    let inputText;
-    while(1){
-        inputText = prompt("Input?");
-        if(inputText != null){
-            let temp = inputText.trim();
-            inputText = temp.toLowerCase();
-            if(inputText == "rock" || inputText == "paper" || inputText == "scissors"){
-                return inputText;
-            }else{
-                console.log("wrong Input! Try again.");
-            }
-        }
 
+let playerWins = 0;
+let computerWins = 0;
+let roundsPlayed = 0;
+const but = document.querySelectorAll('button');
+const result =document.querySelector('#result');
+const score =document.querySelector('#score');
+const winPicture = document.createElement('img');
+const losePicture = document.createElement('img');
+const finish = document.getElementById('finish');
+const rockPicture = document.createElement('img');
+const paperPicture = document.createElement('img');
+const scissorsPicture = document.createElement('img');
+const reset = document.createElement('button');
+
+finish.appendChild(winPicture);
+finish.appendChild(losePicture)
+winPicture.style.display = 'none';
+losePicture.style.display = 'none';
+winPicture.src = "/img/Win.png";
+losePicture.src = "/img/Lost.png";
+rockPicture.src = "/img/Rock.png";
+paperPicture.src = "/img/Paper.png";
+scissorsPicture.src = "/img/Scissors.png";
+reset.textContent = "Reset";
+
+
+
+
+but.forEach(button => button.addEventListener('click', () => {
+    if(roundsPlayed == 5){
+        (computerWins< playerWins) ? winPicture.style.display = 'block': losePicture.style.display = 'block';
+        finish.appendChild(reset);
+        return;
     }
-}
+    result.textContent = playRound(button.id);
+    score.textContent = "Player " + playerWins + " : " + computerWins + " Computer";
+}));
 
-function game(){ //logic to play 5 rounds and posts the result
-    let playerScore = 0;
-    let computerScore = 0;
-    console.log("Game begins!")
-    while(playerScore<5 && computerScore<5){
-        let ergebnis = playRound();
-        switch(ergebnis){
-            case 0: 
-                playerScore++;
-                break;
-            case 1: 
-                computerScore++;
-                break;
-            case 2:
-                console.log("Same Picked. Draw!");
-                break;
-        }
-
-        console.log(playerScore + " Player vs Computer " + computerScore);
-    }
-    let winner = (playerScore == 5) ? "Player won!" : "Player lost!";
-    console.log(winner);
-
-}
-
-game();
+reset.addEventListener('click', () => {
+    playerWins = 0;
+    computerWins = 0;
+    roundsPlayed = 0;
+    score.textContent = "Player " + playerWins + " : " + computerWins + " Computer";
+    winPicture.style.display = 'none';
+    losePicture.style.display = 'none';
+    result.textContent = "";
+});
